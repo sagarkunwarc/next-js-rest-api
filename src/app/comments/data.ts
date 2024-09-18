6d6cd7f044f57628a91caa38e2f3f64ca546f7f7
sagarkunwar <sagar.kunwar24@gmail.com>
2024-09-18 15:13:30 -0400

    htpp method and mock data for get

---
+++ src/app/comments/data.ts
@@ -0,0 +1,7 @@
+export const comments = [
+  { id: 1, text: "comment 1" },
+  { id: 2, text: "comment 2" },
+  { id: 3, text: "comment 3" },
+  { id: 4, text: "comment 4" },
+  { id: 5, text: "comment 5" },
+];
